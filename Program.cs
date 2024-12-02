b0f076f8439cad9c307a1e235c6048aea2d1c110
Sebastian Freyles <freylessebastian@gmail.com>
2024-12-02 15:22:20 -0500

    Actualización 1.1 - Primera entrada

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+public class Calciuladora
+{
+
+  public static void Main(string[] args)
+  {
+
+    Console.Write("Ingrese Su Primer Numero: ");
+
+    string entrada1 = Console.ReadLine();
+
+    double num1 = double.Parse(entrada1);
+
+switch (operacion)
+
+
+
+
+}
+
+
+
+}
